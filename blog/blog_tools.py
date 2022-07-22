b41985a1ce084eb69c16629b5acb1f3cbeaeac2e
Roman Smirnov <graff049@gmail.com>
2022-07-22 16:55:29 +0300

    feature(serialize_post_optimized): add new func

--- blog/blog_tools.py
+++ blog/blog_tools.py
@@ -1,13 +1,14 @@
 """Module, that manipulates with database objects."""
+
 from blog.models import Comment, Post
 
 
 def get_related_posts_count(tag):
-    return tag.tags_amount
+    return tag.posts__count
 
 
 def get_likes_count(post):
-    return post.likes_amount
+    return post.likes__count
 
 
 def serialize_post(post):
@@ -16,6 +17,20 @@ def serialize_post(post):
         'teaser_text': post.text[:200],
         'author': post.author.username,
         'comments_amount': len(Comment.objects.filter(post=post)),
+        'image_url': post.image.url if post.image else None,
+        'published_at': post.published_at,
+        'slug': post.slug,
+        'tags': [serialize_tag(tag) for tag in post.tags.all()],
+        'first_tag_title': post.tags.all()[0].title,
+    }
+
+
+def serialize_post_optimized(post):
+    return {
+        'title': post.title,
+        'teaser_text': post.text[:200],
+        'author': post.author.username,
+        'comments_amount': post.comments__count,
         'image_url': post.image.url if post.image else None,
         'published_at': post.published_at,
         'slug': post.slug,
