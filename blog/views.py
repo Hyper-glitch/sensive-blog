5127de56e87da5bf13d77443b69d38f0b3485674
Roman Smirnov <graff049@gmail.com>
2022-07-22 11:43:28 +0300

    refactor(funcs): move all extra funcs to new module

--- blog/views.py
+++ blog/views.py
@@ -1,34 +1,10 @@
 from django.shortcuts import render
+
+from blog.blog_tools import get_related_posts_count, serialize_post, serialize_tag
 from blog.models import Comment, Post, Tag
 
 
-def get_related_posts_count(tag):
-    return tag.posts.count()
-
-
-def serialize_post(post):
-    return {
-        'title': post.title,
-        'teaser_text': post.text[:200],
-        'author': post.author.username,
-        'comments_amount': len(Comment.objects.filter(post=post)),
-        'image_url': post.image.url if post.image else None,
-        'published_at': post.published_at,
-        'slug': post.slug,
-        'tags': [serialize_tag(tag) for tag in post.tags.all()],
-        'first_tag_title': post.tags.all()[0].title,
-    }
-
-
-def serialize_tag(tag):
-    return {
-        'title': tag.title,
-        'posts_with_tag': len(Post.objects.filter(tags=tag)),
-    }
-
-
 def index(request):
-
     most_popular_posts = []  # TODO. Как это посчитать?
 
     fresh_posts = Post.objects.order_by('published_at')
