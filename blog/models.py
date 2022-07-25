d35f304597d86718ec3149d5cee8cde085f1e887
Roman Smirnov <graff049@gmail.com>
2022-07-25 16:51:13 +0300

    refactor(circular_import): move custom managers to avoid circular_import

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,44 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.db.models import Count
 from django.urls import reverse
 
-from blog.custom_managers import PostQuerySet, TagQuerySet
+
+class PostQuerySet(models.QuerySet):
+
+    def year(self, year):
+        """
+        Manager that filter posts by year.
+        :params year: value for filtering by.
+        :return: filtered posts.
+        """
+        posts_at_year = self.filter(published_at__year=year).order_by('published_at')
+        return posts_at_year
+
+    def popular(self):
+        return self.annotate(likes_count=Count('likes')).order_by('-likes_count')
+
+    def fetch_with_comments_count(self):
+        most_popular_posts_ids = [post.id for post in self]
+        posts_with_comments = Post.objects.filter(id__in=most_popular_posts_ids).annotate(
+            comments_count=Count('comments'))
+        ids_and_comments = posts_with_comments.values_list('id', 'comments_count')
+        count_for_id = dict(ids_and_comments)
+
+        for post in self:
+            post.comments_count = count_for_id[post.id]
+        return self
+
+
+class TagQuerySet(models.QuerySet):
+
+    def popular(self):
+        """
+        Manager that count tags on posts.
+        :return: popular tags.
+        """
+        popular_tags = self.annotate(tags_amount=Count('posts')).order_by('-tags_amount')
+        return popular_tags
 
 
 class Post(models.Model):
