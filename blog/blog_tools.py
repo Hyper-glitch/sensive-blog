bcca7268998f6273646fa831e0fcc251f01e4631
Roman Smirnov <graff049@gmail.com>
2022-07-26 14:28:11 +0300

    feature(optimized): use serialize_tag_optimized func

--- blog/blog_tools.py
+++ blog/blog_tools.py
@@ -1,5 +1,4 @@
 """Module, that manipulates with database objects."""
-from django.db.models import Count
 
 from blog.models import Comment, Post
 
@@ -35,12 +34,19 @@ def serialize_post_optimized(post):
         'image_url': post.image.url if post.image else None,
         'published_at': post.published_at,
         'slug': post.slug,
-        'tags': [serialize_tag(tag) for tag in post.tags.all().posts()],
+        'tags': [serialize_tag_optimized(tag) for tag in post.tags.all()],
         'first_tag_title': post.tags.all()[0].title,
     }
 
 
 def serialize_tag(tag):
+    return {
+        'title': tag.title,
+        'posts_with_tag': len(Post.objects.filter(tags=tag)),
+    }
+
+
+def serialize_tag_optimized(tag):
     return {
         'title': tag.title,
         'posts_with_tag': tag.posts_amount,
