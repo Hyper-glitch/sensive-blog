8ae16986349dc48d40e3dbcf390373273f07f3b7
Roman Smirnov <graff049@gmail.com>
2022-07-26 18:35:12 +0300

    feature(optimize): optimize queries in post_detail view

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 
-from blog.blog_tools import serialize_post, serialize_tag, serialize_post_optimized, serialize_tag_optimized, \
-    get_most_popular_posts
+from blog.blog_tools import (
+    serialize_post_optimized, serialize_tag_optimized, get_most_popular_posts,
+)
 from blog.models import Comment, Post, Tag
 
 
@@ -24,12 +25,13 @@ def index(request):
 
 
 def post_detail(request, slug):
-    top_tags_amount = 5
+    top_obj_amount = 5
     serialized_comments = []
-
-    post = Post.objects.likes().get(slug=slug)
-    comments = Comment.objects.filter(post=post)
-    related_tags = post.tags.all()
+    most_popular_posts, posts_prefetch = get_most_popular_posts(top_obj_amount)
+    post = Post.objects.prefetch_related('author').likes().get(slug=slug)
+    comments = Comment.objects.prefetch_related('author').filter(post=post)
+    most_popular_tags = Tag.objects.popular()[:top_obj_amount]
+    related_tags = post.tags.popular()
 
     for comment in comments:
         serialized_comments.append({
@@ -47,15 +49,13 @@ def post_detail(request, slug):
         'image_url': post.image.url if post.image else None,
         'published_at': post.published_at,
         'slug': post.slug,
-        'tags': [serialize_tag(tag) for tag in related_tags],
+        'tags': [serialize_tag_optimized(tag) for tag in related_tags],
     }
-    most_popular_tags = Tag.objects.popular()[:top_tags_amount]
-    most_popular_posts = []  # TODO. Как это посчитать?
     context = {
         'post': serialized_post,
-        'popular_tags': [serialize_tag(tag) for tag in most_popular_tags],
+        'popular_tags': [serialize_tag_optimized(tag) for tag in most_popular_tags],
         'most_popular_posts': [
-            serialize_post(post) for post in most_popular_posts
+            serialize_post_optimized(post) for post in most_popular_posts
         ],
     }
     return render(request, 'post-details.html', context)
