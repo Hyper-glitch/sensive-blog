dd74f6f4c57bfb90ca504d0333c6e311a1d05a56
Roman Smirnov <graff049@gmail.com>
2022-07-27 10:56:51 +0300

    refactor(delete): remove extra code

--- blog/blog_tools.py
+++ blog/blog_tools.py
@@ -1,7 +1,7 @@
 """Module, that manipulates with database objects."""
 from django.db.models import Prefetch, Count
 
-from blog.models import Comment, Post, Tag
+from blog.models import Post, Tag
 
 
 def get_related_posts_count(tag):
@@ -17,7 +17,7 @@ def serialize_post(post):
         'title': post.title,
         'teaser_text': post.text[:200],
         'author': post.author.username,
-        'comments_amount': len(Comment.objects.filter(post=post)),
+        'comments_amount': post.comments_count,
         'image_url': post.image.url if post.image else None,
         'published_at': post.published_at,
         'slug': post.slug,
@@ -26,28 +26,7 @@ def serialize_post(post):
     }
 
 
-def serialize_post_optimized(post):
-    return {
-        'title': post.title,
-        'teaser_text': post.text[:200],
-        'author': post.author.username,
-        'comments_amount': post.comments_count,
-        'image_url': post.image.url if post.image else None,
-        'published_at': post.published_at,
-        'slug': post.slug,
-        'tags': [serialize_tag_optimized(tag) for tag in post.tags.all()],
-        'first_tag_title': post.tags.all()[0].title,
-    }
-
-
 def serialize_tag(tag):
-    return {
-        'title': tag.title,
-        'posts_with_tag': len(Post.objects.filter(tags=tag)),
-    }
-
-
-def serialize_tag_optimized(tag):
     return {
         'title': tag.title,
         'posts_with_tag': tag.posts_amount,
