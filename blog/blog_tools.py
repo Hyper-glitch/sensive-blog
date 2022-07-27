1b69e2df46e65a136a8ebb201578aa923b696719
Roman Smirnov <graff049@gmail.com>
2022-07-27 12:34:25 +0300

    refactor(hinting): add type hinting

--- blog/blog_tools.py
+++ blog/blog_tools.py
@@ -1,18 +1,12 @@
 """Module, that manipulates with database objects."""
-from django.db.models import Prefetch, Count
+from typing import Tuple
+
+from django.db.models import Prefetch, Count, QuerySet
 
 from blog.models import Post, Tag
 
 
-def get_related_posts_count(tag):
-    return tag.posts__count
-
-
-def get_likes_count(post):
-    return post.likes__count
-
-
-def serialize_post(post):
+def serialize_post(post: Post) -> dict:
     return {
         'title': post.title,
         'teaser_text': post.text[:200],
@@ -26,16 +20,22 @@ def serialize_post(post):
     }
 
 
-def serialize_tag(tag):
+def serialize_tag(tag: Tag) -> dict:
     return {
         'title': tag.title,
         'posts_with_tag': tag.posts_amount,
     }
 
 
-def get_most_popular_posts(top_obj_amount):
+def get_most_popular_posts(top_obj_amount: int) -> Tuple[QuerySet, Prefetch]:
+    """
+    Annotate and prefetch posts with likes and authors.
+    :params: amount of slicing posts.
+    :return: queryset ordered by likes, with authors and posts with comments amount; post's prefetch.
+    """
     posts_prefetch = Prefetch('tags', queryset=Tag.objects.annotate(posts_amount=Count('posts')))
     popular_posts = Post.objects.likes().order_by('-likes_count').prefetch_related('author').prefetch_related(
         posts_prefetch)[:top_obj_amount].fetch_with_comments_count()
     most_popular_posts = popular_posts[:top_obj_amount]
+
     return most_popular_posts, posts_prefetch
