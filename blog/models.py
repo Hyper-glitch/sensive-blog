fc66aa8cad7e457cc8f60ab2b4309bf955e47d4b
Roman Smirnov <graff049@gmail.com>
2022-07-29 16:13:01 +0300

    refactor(move): move code that related to model

--- blog/models.py
+++ blog/models.py
@@ -45,6 +45,18 @@ class PostQuerySet(models.QuerySet):
             post.comments_count = count_for_id[post.id]
         return self
 
+    def get_most_popular_posts(self, posts_prefetch, top_obj_amount):
+        """
+        Manager that annotate and prefetch posts with likes and authors.
+        :params posts_prefetch: amount of slicing posts.
+        :params top_obj_amount: amount of slicing posts.
+        :return: queryset ordered by likes, with authors and posts with comments amount; post's prefetch.
+        """
+        popular_posts = self.likes().order_by('-likes_count').prefetch_related('author').prefetch_related(
+            posts_prefetch)[:top_obj_amount].fetch_with_comments_count()
+        most_popular_posts = popular_posts[:top_obj_amount]
+        return most_popular_posts
+
 
 class TagQuerySet(models.QuerySet):
     """Custom Tag queryset manager."""
