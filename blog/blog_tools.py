a71a0e5b72951d658492c18b34464708f8e012d8
Roman Smirnov <graff049@gmail.com>
2022-07-29 15:03:24 +0300

    refactor(move): move code that related with view

--- blog/blog_tools.py
+++ blog/blog_tools.py
@@ -6,27 +6,6 @@ from django.db.models import Prefetch, Count, QuerySet
 from blog.models import Post, Tag
 
 
-def serialize_post(post: Post) -> dict:
-    return {
-        'title': post.title,
-        'teaser_text': post.text[:200],
-        'author': post.author.username,
-        'comments_amount': post.comments_count,
-        'image_url': post.image.url if post.image else None,
-        'published_at': post.published_at,
-        'slug': post.slug,
-        'tags': [serialize_tag(tag) for tag in post.tags.all()],
-        'first_tag_title': post.tags.all()[0].title,
-    }
-
-
-def serialize_tag(tag: Tag) -> dict:
-    return {
-        'title': tag.title,
-        'posts_with_tag': tag.posts_amount,
-    }
-
-
 def get_most_popular_posts(top_obj_amount: int) -> Tuple[QuerySet, Prefetch]:
     """
     Annotate and prefetch posts with likes and authors.
